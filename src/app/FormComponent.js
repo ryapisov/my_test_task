db2518ada540c882e381f19c14d97a3a5b7fd6d7
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-12-07 16:27:09 +0300

    change FormComponent add hook useInput to the form

--- src/app/FormComponent.js
+++ src/app/FormComponent.js
@@ -1,102 +1,57 @@
 import React from 'react'
 
+const useInput = (initialValue) => {
+  const [value, setValue] = React.useState(initialValue)
+  const [isDirty, setDirty] = React.useState(false)
+  const onChange = (e) => {
+    const {value} = e.target
+    setValue(value)
+  }
+  const onBlur = (e) => {
+    setDirty(true)
+  }
+  return {value, onChange, onBlur, isDirty}
+}
+
 function FormComponent() {
-  const [username, setUserName] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [userNameDirty, setUserNameDirty] = React.useState(false)
-  const [passwordDirty, setPasswordDirty] = React.useState(false)
-  const [userNameError, setUserNameError] = React.useState('имя пользователя не должно быть пустым')
-  const [passwordError, setPasswordError] = React.useState('пароль не может быть пустым')
-  const [formValid, setFormValid] = React.useState(false)
-  
+  const username = useInput('')
+  const password = useInput('')
+
   /* 
     наблюдает за кнопкой, если
     ошибок нет, делает кнопку активной
   */
-  React.useEffect(() => {
-    if(userNameError || passwordError){
-      setFormValid(false)
-    }else{
-      setFormValid(true)
-    }
-  }, [userNameError, passwordError])
-
-  /*
-    установка значения в поле "input"
-    а также валидация значения username,
-  */
-  const usernameHandler = (e) =>{
-    const {value} = e.target
-    setUserName(value)
-    const re = /^[\w+_@.]{1,150}$/
-    const isValid = re.test(String(value).toLocaleLowerCase())
-    if (isValid){
-      setUserNameError("")
-    }
-    else {
-      setUserNameError("не корректный пользователь")
-    }
-  }
-
-  /*
-    установка значения в поле "input"
-    а также валидация password
-  */
-  const passwordHandler = (e) =>{
-    const {value} = e.target
-    setPassword(value)
-    const re = /^[\w=.@$#><&!]{1,128}$/
-    const isValid = re.test(String(value).toLocaleLowerCase())
-    if(isValid){
-      setPasswordError("")
-    }
-    else {
-      setPasswordError("не корректный пароль")
-    }
-  }
-
-  /*
-    при потере фокуса в текстовом поле "input",
-    заставляет позаказать уведомление о том, 
-    что поле пусто.
-  */
-  const blurHandler = (e) => {
-    const {name} = e.target
-    switch (name) {
-      case 'username':
-        setUserNameDirty(true)
-        break
-      case 'password':
-        setPasswordDirty(true)
-        break
-      default:
-        break
-    }
-  }
+  // React.useEffect(() => {
+  //   if(userNameError || passwordError){
+  //     setFormValid(false)
+  //   }else{
+  //     setFormValid(true)
+  //   }
+  // }, [userNameError, passwordError])
 
   return(
     <form>
       <h2>форма входа</h2>
-      {(userNameDirty && userNameError ) && <>{userNameError}</>}
+      { username.isDirty  && <>поле username не может быть пустым</>}
       <input 
-        value={username} 
-        onBlur={(e) => blurHandler(e)}
-        onChange={(e) => usernameHandler(e)}
+        value={username.value} 
+        onBlur={(e)=> username.onBlur(e)}
+        onChange={(e)=> username.onChange(e)}
         placeholder="Пользователь"
         name="username"
         type="text"
       />
-      {(passwordDirty && passwordError ) && <>{passwordError}</>}
+      { password.isDirty  && <>поле password не может быть пустым</>}
       <input 
-        value={password}
-        onBlur={(e) => blurHandler(e)}
-        onChange={(e) => passwordHandler(e)}
+        value={password.value}
+        onBlur={(e)=> password.onBlur(e)}
+        onChange={(e)=> password.onChange(e)}
         placeholder="Пароль"
         name="password"
         type="password"
       />
       <button
-        disabled={!formValid}
+       // disabled={!formValid}
         type="submit"
       >Войти
       </button>
