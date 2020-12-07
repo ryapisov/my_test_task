b5cc27bc3e65b5e4001e6787e085e1bd53814cac
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-12-07 16:51:09 +0300

    change FormComponent add hook useValidation to the form

--- src/app/FormComponent.js
+++ src/app/FormComponent.js
@@ -1,8 +1,35 @@
 import React from 'react'
 
-const useInput = (initialValue) => {
+const useValidation = (value, validations) => {
+  const [isEmpty, setEmpty] = React.useState(true)
+  const [minLengthError, setMinLengthError] = React.useState(false)
+
+  React.useEffect(()=>{
+    for (const validation in validations){
+      switch(validation){
+        case 'minLength':
+          value.length < validations[validation]
+            ? setMinLengthError(true)
+            : setMinLengthError(false)
+          break
+        case 'isEmpty':
+          value ? setEmpty(false) : setEmpty(true)
+          break
+      }
+    }
+  }, [value])
+
+  return {
+    isEmpty,
+    minLengthError
+  }
+}
+
+const useInput = (initialValue, validations) => {
   const [value, setValue] = React.useState(initialValue)
   const [isDirty, setDirty] = React.useState(false)
+  const valid = useValidation(value, validations)
+
   const onChange = (e) => {
     const {value} = e.target
     setValue(value)
@@ -10,52 +37,50 @@ const useInput = (initialValue) => {
   const onBlur = (e) => {
     setDirty(true)
   }
-  return {value, onChange, onBlur, isDirty}
+  return {
+    value,
+    onChange,
+    onBlur,
+    isDirty,
+    ...valid
+  }
 }
 
 function FormComponent() {
-  const username = useInput('')
-  const password = useInput('')
-
-  /* 
-    наблюдает за кнопкой, если
-    ошибок нет, делает кнопку активной
-  */
-  // React.useEffect(() => {
-  //   if(userNameError || passwordError){
-  //     setFormValid(false)
-  //   }else{
-  //     setFormValid(true)
-  //   }
-  // }, [userNameError, passwordError])
-
-  return(
-    <form>
-      <h2>форма входа</h2>
-      { username.isDirty  && <>поле username не может быть пустым</>}
-      <input 
-        value={username.value} 
-        onBlur={(e)=> username.onBlur(e)}
-        onChange={(e)=> username.onChange(e)}
-        placeholder="Пользователь"
-        name="username"
-        type="text"
-      />
-      { password.isDirty  && <>поле password не может быть пустым</>}
-      <input 
-        value={password.value}
-        onBlur={(e)=> password.onBlur(e)}
-        onChange={(e)=> password.onChange(e)}
-        placeholder="Пароль"
-        name="password"
-        type="password"
-      />
-      <button
-       // disabled={!formValid}
-        type="submit"
-      >Войти
-      </button>
-    </form>
+  const username = useInput('', {isEmpty:true, minLength:3, isUserName:true})
+  const password = useInput('', {isEmpty:true, minLength:5, maxLength:30})
+  
+  return (
+    <div>
+      <form>
+        {username.isDirty && username.isEmpty && <>username неможет быть пустым</>}
+        {username.isDirty && username.minLengthError && <>username длинна менее 5 символов</>}
+        <input 
+          value={username.value} 
+          onBlur={(e)=> username.onBlur(e)}
+          onChange={(e)=> username.onChange(e)}
+          placeholder="Пользователь"
+          name="username"
+          type="text"
+        />
+        {password.isDirty && password.isEmpty && <>password не может быть пустым</>}
+        {password.isDirty && password.minLengthError && <>password пароль длинна менее 5 символов</>}
+        <input 
+          value={password.value} 
+          onBlur={(e)=> password.onBlur(e)}
+          onChange={(e)=> password.onChange(e)}
+          placeholder="Пароль"
+          name="password"
+          type="password"
+        />
+        <button 
+          disabled={username.minLengthError || password.minLengthError} 
+          type="submit"
+          onClick={()=>{}}
+        >Войти
+        </button>
+      </form>
+    </div>
   )
 }
 
